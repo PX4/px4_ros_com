44642c9c1ef92f05b132a6c2a315ab0d3627a070
TSC21 <n.marques21@hotmail.com>
2018-09-13 14:50:34 +0100

    add micro-RTPS agent code build to CMakeLists.txt

--- cmake/GenerateMicroRTPSAgent.cmake
+++ cmake/GenerateMicroRTPSAgent.cmake
@@ -23,9 +23,32 @@ else()
     message(FATAL_ERROR "RTPS msg ID yaml file \"${PX4_FIRMWARE_MSG_DIR}/tools/uorb_rtps_message_ids.yaml\" not found!")
 endif()
 
+# Set the micro-RTPS agent code dir
+set(MICRORTPS_AGENT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/src/micrortps_agent)
+
+# Set the list of files to be compiled
+set(MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/microRTPS_agent.cpp)
+list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/microRTPS_transport.h)
+list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/microRTPS_transport.cpp)
+list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/RtpsTopics.h)
+list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/RtpsTopics.cpp)
+foreach(topic ${ROS_UORB_NAMES})
+    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_.cpp)
+    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_Publisher.cpp)
+    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_Publisher.h)
+    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_Subscriber.cpp)
+    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_Subscriber.h)
+    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_PubSubTypes.cpp)
+    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_PubSubTypes.h)
+endforeach()
+
 # Generates the micro-RTPS agent code after the IDL msgs are generated
 add_custom_command(
-    TARGET ${PROJECT_NAME}
+    OUTPUT ${MICRORTPS_AGENT_FILES}
+    DEPENDS ${PROJECT_NAME}
+        ${PYTHON_EXECUTABLE} ${PX4_FIRMWARE_MSG_DIR}/tools/generate_microRTPS_bridge.py
+        $ENV{FASTRTPSGEN_DIR}
+        ${ROS_UORB_MSGS_DIR}
     COMMAND ${PYTHON_EXECUTABLE} ${PX4_FIRMWARE_MSG_DIR}/tools/generate_microRTPS_bridge.py
         --fastrtpsgen-dir $ENV{FASTRTPSGEN_DIR}
         --fastrtpsgen-include ${CMAKE_CURRENT_BINARY_DIR}/rosidl_generator_dds_idl/
@@ -34,9 +57,11 @@ add_custom_command(
         --topic-msg-dir ${CMAKE_CURRENT_SOURCE_DIR}/msg
         --urtps-templates-dir "templates"
         --agent
-        --agent-outdir ${CMAKE_CURRENT_SOURCE_DIR}/src/micrortps_agent
+        --agent-outdir ${MICRORTPS_AGENT_DIR}
         --package ${PROJECT_NAME}
         --idl-dir ${CMAKE_CURRENT_BINARY_DIR}/rosidl_generator_dds_idl/${PROJECT_NAME}/msg/dds_opensplice
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    COMMENT "Generating micro-RTPS agent..."
+    COMMENT "Generating micro-RTPS agent code..."
 )
+
+set(MICRORTPS_AGENT_FILES "${MICRORTPS_AGENT_FILES}" CACHE INTERNAL "MICRORTPS_AGENT_FILES")

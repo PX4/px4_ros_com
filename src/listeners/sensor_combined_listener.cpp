ff14fe36697e39d2d947a592af63017f9f4e250b
TSC21 <n.marques21@hotmail.com>
2018-09-13 16:21:22 +0100

    add sensor combined listener example

---
+++ src/listeners/sensor_combined_listener.cpp
@@ -0,0 +1,86 @@
+/****************************************************************************
+*
+* Copyright 2017 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+*           2018 PX4 Pro Development Team. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+* this list of conditions and the following disclaimer in the documentation
+* and/or other materials provided with the distribution.
+*
+* 3. Neither the name of the copyright holder nor the names of its contributors
+* may be used to endorse or promote products derived from this software without
+* specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+****************************************************************************/
+
+/**
+ * @brief Sensor Combined uORB topic listener
+ * @file sensor_combined_listener.cpp
+ * @addtogroup examples
+ * @author Nuno Marques <nuno.marques@dronesolutions.io>
+ * @author Vicente Monge
+ */
+
+#include <rclcpp/rclcpp.hpp>
+#include <px4_ros_com/msg/sensor_combined.hpp>
+
+using namespace px4_ros_com::msg;
+
+/**
+ * @brief Sensor Combined uORB topic data callback
+ */
+class SensorCombinedListener : public rclcpp::Node
+{
+public:
+	explicit SensorCombinedListener() : Node("sensor_combined_listener"){
+		auto callback =
+		[this](const SensorCombined::SharedPtr msg)->void
+		{
+			std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+			std::cout << "RECEIVED DATA ON SENSOR COMBINED" << std::endl;
+			std::cout << "================================" << std::endl;
+			std::cout << "gyro_rad[0]: " << msg->gyro_rad[0] << std::endl;
+			std::cout << "gyro_rad[1]: " << msg->gyro_rad[1] << std::endl;
+			std::cout << "gyro_rad[2]: " << msg->gyro_rad[2] << std::endl;
+			std::cout << "gyro_integral_dt: " << msg->gyro_integral_dt << std::endl;
+			std::cout << "accelerometer_timestamp_relative: " << msg->accelerometer_timestamp_relative << std::endl;
+			std::cout << "accelerometer_m_s2[0]: " << msg->accelerometer_m_s2[0] << std::endl;
+			std::cout << "accelerometer_m_s2[1]: " << msg->accelerometer_m_s2[1] << std::endl;
+			std::cout << "accelerometer_m_s2[2]: " << msg->accelerometer_m_s2[2] << std::endl;
+			std::cout << "accelerometer_integral_dt: " << msg->accelerometer_integral_dt << std::endl;
+		};
+		subscription_ = this->create_subscription<SensorCombined>("SensorCombined_topic", callback);
+	}
+
+private:
+	rclcpp::Subscription<SensorCombined>::SharedPtr subscription_;
+};
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Starting demo..." << std::endl;
+	setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<SensorCombinedListener>());
+
+	rclcpp::shutdown();
+	return 0;
+}

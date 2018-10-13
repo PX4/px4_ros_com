3aec98f0fbfdb4e725eacc419bf901ed6922967f
TSC21 <n.marques21@hotmail.com>
2018-10-13 21:39:18 +0100

    fix some minor details on GenerateMicroRTPSAgent.cmake and RtpsTopics.cpp.template so to be able to build

--- cmake/GenerateMicroRTPSAgent.cmake
+++ cmake/GenerateMicroRTPSAgent.cmake
@@ -35,11 +35,14 @@
 # Depends on:
 # - PROJECT_NAME
 # - PX4_FIRMWARE_MSG_DIR/tools/uorb_rtps_message_ids.yaml
+# - PX4_FIRMWARE_MSG_DIR/tools/uorb_rtps_classifier.py
 # - FASTRTPSGEN_DIR
 # - ROS_UORB_MSGS_DIR
 
 # Add the python scripts dir from PX4 msg/tools to PYTHONPATH
 set(ENV{PYTHONPATH} "$ENV{PYTHONPATH}:${PX4_FIRMWARE_MSG_DIR}/tools/")
+
+set(PACKAGE_SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR})
 
 # Check if the RTPS ID's mapper yaml file exists and if yes, change the msg naming to PascalCase
 if(EXISTS ${PX4_FIRMWARE_MSG_DIR}/tools/uorb_rtps_message_ids.yaml)
@@ -47,11 +50,38 @@ if(EXISTS ${PX4_FIRMWARE_MSG_DIR}/tools/uorb_rtps_message_ids.yaml)
 
     # Parse the RTPS IDs for each uORB msg into the counterparts for ROS
     execute_process(
-        COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/scripts/PX42ROSRTPSIds.py
+        COMMAND ${PYTHON_EXECUTABLE} ${PACKAGE_SRC_DIR}/scripts/PX42ROSRTPSIds.py
             --input-file ${PX4_FIRMWARE_MSG_DIR}/tools/uorb_rtps_message_ids.yaml
             --output-file ${RTPS_ID_YAML_FILE}
         )
     message(STATUS "RTPS msg ID yaml file: ${RTPS_ID_YAML_FILE}")
+
+    # Create list of messages to send
+    set(CONFIG_RTPS_SEND_TOPICS)
+    execute_process(
+        COMMAND ${PYTHON_EXECUTABLE} ${PX4_FIRMWARE_MSG_DIR}/tools/uorb_rtps_classifier.py
+            --receive # the msgs the client receives, are the messages the agent sends
+            --topic-msg-dir ${PACKAGE_SRC_DIR}/msg
+            --rtps-ids-file templates/uorb_rtps_message_ids.yaml
+        OUTPUT_VARIABLE CONFIG_RTPS_SEND_TOPICS
+    )
+
+    STRING(REGEX REPLACE ", " ";" CONFIG_RTPS_SEND_TOPICS "${CONFIG_RTPS_SEND_TOPICS}")
+    STRING(REGEX REPLACE "\n" "" CONFIG_RTPS_SEND_TOPICS "${CONFIG_RTPS_SEND_TOPICS}")
+
+    # Create list of messages to receive
+    set(CONFIG_RTPS_RECEIVE_TOPICS)
+    execute_process(
+        COMMAND ${PYTHON_EXECUTABLE} ${PX4_FIRMWARE_MSG_DIR}/tools/uorb_rtps_classifier.py
+            --send  # the msgs the client sends, are the messages the agent receives
+            --topic-msg-dir ${PACKAGE_SRC_DIR}/msg
+            --rtps-ids-file templates/uorb_rtps_message_ids.yaml
+        OUTPUT_VARIABLE CONFIG_RTPS_RECEIVE_TOPICS
+    )
+
+    STRING(REGEX REPLACE ", " ";" CONFIG_RTPS_RECEIVE_TOPICS "${CONFIG_RTPS_RECEIVE_TOPICS}")
+    STRING(REGEX REPLACE "\n" "" CONFIG_RTPS_RECEIVE_TOPICS "${CONFIG_RTPS_RECEIVE_TOPICS}")
+
 else()
     message(FATAL_ERROR "RTPS msg ID yaml file \"${PX4_FIRMWARE_MSG_DIR}/tools/uorb_rtps_message_ids.yaml\" not found!")
 endif()
@@ -65,28 +95,34 @@ list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/microRTPS_transport.h)
 list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/microRTPS_transport.cpp)
 list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/RtpsTopics.h)
 list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/RtpsTopics.cpp)
-foreach(topic ${ROS_UORB_NAMES})
+
+set(ALL_TOPIC_NAMES ${CONFIG_RTPS_SEND_TOPICS} ${CONFIG_RTPS_RECEIVE_TOPICS})
+foreach(topic ${ALL_TOPIC_NAMES})
     list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_.cpp)
-    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_Publisher.cpp)
-    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_Publisher.h)
-    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_Subscriber.cpp)
-    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_Subscriber.h)
     list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_PubSubTypes.cpp)
     list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_PubSubTypes.h)
+endforeach()
+
+foreach(topic ${CONFIG_RTPS_RECEIVE_TOPICS})  # received topics are to be published to the DDS layer
+    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_Publisher.cpp)
+    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_Publisher.h)
+endforeach()
+
+foreach(topic ${CONFIG_RTPS_SEND_TOPICS}) # advertised topics should first be subscribed to the DDS layer
+    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_Subscriber.cpp)
+    list(APPEND MICRORTPS_AGENT_FILES ${MICRORTPS_AGENT_DIR}/${topic}_Subscriber.h)
 endforeach()
 
 # Generates the micro-RTPS agent code after the IDL msgs are generated
 add_custom_command(
     OUTPUT ${MICRORTPS_AGENT_FILES}
     DEPENDS ${PROJECT_NAME}
-        ${PYTHON_EXECUTABLE} ${PX4_FIRMWARE_MSG_DIR}/tools/generate_microRTPS_bridge.py
+        ${PX4_FIRMWARE_MSG_DIR}/tools/generate_microRTPS_bridge.py
         $ENV{FASTRTPSGEN_DIR}
         ${ROS_UORB_MSGS_DIR}
     COMMAND ${PYTHON_EXECUTABLE} ${PX4_FIRMWARE_MSG_DIR}/tools/generate_microRTPS_bridge.py
         --fastrtpsgen-dir $ENV{FASTRTPSGEN_DIR}
         --fastrtpsgen-include ${CMAKE_CURRENT_BINARY_DIR}/rosidl_generator_dds_idl/
-        --send ${ROS_UORB_MSGS_DIR}
-        --receive ${ROS_UORB_MSGS_DIR}
         --topic-msg-dir ${CMAKE_CURRENT_SOURCE_DIR}/msg
         --urtps-templates-dir templates
         --rtps-ids-file templates/uorb_rtps_message_ids.yaml

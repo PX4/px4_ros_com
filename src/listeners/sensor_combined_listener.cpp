6d883ba25a7ed78e31e2ea4254e000a487bcc014
TSC21 <n.marques21@hotmail.com>
2018-10-28 21:55:17 +0000

    simplify listener example

--- src/listeners/sensor_combined_listener.cpp
+++ src/listeners/sensor_combined_listener.cpp
@@ -44,97 +44,60 @@
 
 #include "px4_ros_com/SensorCombined.h"
 
-static constexpr auto kDefaultSensorCombinedPubTopic = "sensor_combined_topic_feedback";
-static constexpr auto kDefaultSensorCombinedSubTopic = "sensor_combined_topic";
+static constexpr auto kDefaultSensorCombinedPubTopic = "SensorCombined_topic_feedback";
+static constexpr auto kDefaultSensorCombinedSubTopic = "SensorCombined_topic";
 
-class SensorCombinedPublisher {
-public:
-        //! SensorCombinedPublisher object constructor
-        SensorCombinedPublisher();
-        //! SensorCombinedPublisher object destructor
-        ~SensorCombinedPublisher();
+//! sensor_combined publisher (to ROS) naming
+std::string _sensor_combined_pub_topic;
+//! sensor_combined subscriber (from ROS2) naming
+std::string _sensor_combined_sub_topic;
 
-        //! ROS node handler
-        ros::NodeHandle _nh;
-
-private:
-        //! sensor_combined publisher (to ROS) naming
-        std::string _sensor_combined_pub_topic;
-        //! sensor_combined subscriber (from ROS2) naming
-        std::string _sensor_combined_sub_topic;
-
-        //! sensor_combined publisher (to ROS)
-        ros::Publisher _sensor_combined_pub;
-
-        //! sensor_combined subscriber (from ROS2)
-        ros::Subscriber _sensor_combined_sub;
-
-        //! sensor_combined callback (from incoming ROS2 msgs)
-        void sensor_combined_callback(const px4_ros_com::SensorCombined::ConstPtr &msg);
-
-        // Thread related
-        boost::mutex _mutex;
-        boost::shared_ptr<ros::AsyncSpinner> _spinner;
-};      // class SensorCombinedPublisher
-
-SensorCombinedPublisher::SensorCombinedPublisher() :
-        _nh("~"),
-        _sensor_combined_pub_topic(kDefaultSensorCombinedPubTopic),
-        _sensor_combined_sub_topic(kDefaultSensorCombinedSubTopic),
-        _sensor_combined_pub(_nh.advertise<px4_ros_com::SensorCombined>
-                                     (_sensor_combined_pub_topic, 10)),
-        _sensor_combined_sub(_nh.subscribe<px4_ros_com::SensorCombined>
-                                     (_sensor_combined_sub_topic, 10, &SensorCombinedPublisher::sensor_combined_callback, this)),
-        // create AsyncSpinner, run it on all available cores
-     	_spinner(boost::make_shared<ros::AsyncSpinner>(boost::thread::hardware_concurrency()))
-{
-        // start the async spinner with multiple threads to handle callbacks
-        _spinner->start();
-}
-
-SensorCombinedPublisher::~SensorCombinedPublisher() {
-        boost::mutex::scoped_lock lock(_mutex);
-
-        // stop the spinner
-        _spinner->stop();
-
-        // release AsyncSpinner object
-        _spinner.reset();
-}
+//! sensor_combined publisher (to ROS)
+ros::Publisher _sensor_combined_pub;
+//! sensor_combined subscriber (from ROS2)
+ros::Subscriber _sensor_combined_sub;
 
 /**
  * @brief Sensor Combined uORB topic data callback
  */
-void SensorCombinedPublisher::sensor_combined_callback(const px4_ros_com::SensorCombined::ConstPtr& msg)
+void sensor_combined_callback(const px4_ros_com::SensorCombined::ConstPtr& msg)
 {
-        ROS_INFO("Receiving sensor_combined_topic");
-        std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-        std::cout << "RECEIVED DATA FROM SENSOR COMBINED" << std::endl;
-        std::cout << "================================" << std::endl;
-        std::cout << "gyro_rad[0]: " << msg->gyro_rad[0] << std::endl;
-        std::cout << "gyro_rad[1]: " << msg->gyro_rad[1] << std::endl;
-        std::cout << "gyro_rad[2]: " << msg->gyro_rad[2] << std::endl;
-        std::cout << "gyro_integral_dt: " << msg->gyro_integral_dt << std::endl;
-        std::cout << "accelerometer_timestamp_relative: " << msg->accelerometer_timestamp_relative << std::endl;
-        std::cout << "accelerometer_m_s2[0]: " << msg->accelerometer_m_s2[0] << std::endl;
-        std::cout << "accelerometer_m_s2[1]: " << msg->accelerometer_m_s2[1] << std::endl;
-        std::cout << "accelerometer_m_s2[2]: " << msg->accelerometer_m_s2[2] << std::endl;
-        std::cout << "accelerometer_integral_dt: " << msg->accelerometer_integral_dt << std::endl;
-        std::cout << std::endl << "Publishing back..." << std::endl;
-        fflush(stdout);
+	ROS_INFO("Receiving sensor_combined_topic");
+	std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+	std::cout << "RECEIVED DATA FROM SENSOR COMBINED" << std::endl;
+	std::cout << "================================" << std::endl;
+	std::cout << "gyro_rad[0]: " << msg->gyro_rad[0] << std::endl;
+	std::cout << "gyro_rad[1]: " << msg->gyro_rad[1] << std::endl;
+	std::cout << "gyro_rad[2]: " << msg->gyro_rad[2] << std::endl;
+	std::cout << "gyro_integral_dt: " << msg->gyro_integral_dt << std::endl;
+	std::cout << "accelerometer_timestamp_relative: " << msg->accelerometer_timestamp_relative << std::endl;
+	std::cout << "accelerometer_m_s2[0]: " << msg->accelerometer_m_s2[0] << std::endl;
+	std::cout << "accelerometer_m_s2[1]: " << msg->accelerometer_m_s2[1] << std::endl;
+	std::cout << "accelerometer_m_s2[2]: " << msg->accelerometer_m_s2[2] << std::endl;
+	std::cout << "accelerometer_integral_dt: " << msg->accelerometer_integral_dt << std::endl;
+	std::cout << std::endl << "Publishing back..." << std::endl;
+	fflush(stdout);
 
-        _sensor_combined_pub.publish(msg);
+        // republish data in another topic. Can be converted to a std ROS msg instead
+	_sensor_combined_pub.publish(msg);
 }
 
 int main(int argc, char **argv)
 {
-        ros::init(argc, argv, "sensor_combuned_listener_node");
+        // init the node
+	ros::init(argc, argv, "sensor_combined_listener_node");
 
-        // init node
-        auto node_ptr = boost::make_unique<SensorCombinedPublisher>();
+        // create the node handler
+        ros::NodeHandle nh;
 
-        // wait for ROS threads to terminate
-        ros::waitForShutdown();
+        // set the publisher
+        _sensor_combined_pub = nh.advertise<px4_ros_com::SensorCombined>(kDefaultSensorCombinedPubTopic, 100);
 
-        return 0;
+        // set the subscriber
+        _sensor_combined_sub = nh.subscribe(kDefaultSensorCombinedSubTopic, 100, sensor_combined_callback);
+
+        // sping the node
+        ros::spin();
+
+	return 0;
 }

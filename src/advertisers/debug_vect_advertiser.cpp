62c22435047ff5944535dc0606d68c93d0b05f16
TSC21 <n.marques21@hotmail.com>
2020-03-09 19:48:42 +0000

    debug_vect_advertiser: use std::chrono instead of ROS time to set the timestamp

--- src/advertisers/debug_vect_advertiser.cpp
+++ src/advertisers/debug_vect_advertiser.cpp
@@ -55,13 +55,13 @@ public:
 		auto timer_callback =
 		[this]()->void {
 			auto debug_vect = px4_msgs::msg::DebugVect();
-			debug_vect.timestamp = this->now().nanoseconds() * 1E-3;
+			debug_vect.timestamp = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::steady_clock::now()).time_since_epoch().count();
 			std::string name = "test";
 			std::copy(name.begin(), name.end(), debug_vect.name.begin());
 			debug_vect.x = 1.0;
 			debug_vect.y = 2.0;
 			debug_vect.z = 3.0;
-			RCLCPP_INFO(this->get_logger(), "\033[97m Publishing debug_vect: time: %f x:%f y:%f z:%f \033[0m",
+			RCLCPP_INFO(this->get_logger(), "\033[97m Publishing debug_vect: time: %llu x: %f y: %f z: %f \033[0m",
                                 debug_vect.timestamp, debug_vect.x, debug_vect.y, debug_vect.z);
 			this->publisher_->publish(debug_vect);
 		};

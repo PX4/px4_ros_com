40191c79224ba8de4fd5692740f9b05d3eaed8a0
TSC21 <n.marques21@hotmail.com>
2021-05-27 12:41:12 +0200

    sensor_combined_listener.launch.py: add support for new launch API

--- launch/sensor_combined_listener.launch.py
+++ launch/sensor_combined_listener.launch.py
@@ -35,19 +35,23 @@
 """
 Example to launch a sensor_combined listener node.
 
-.. note::
-    The `launch_ros` package that provides the `launch` API is only available from ROS2 Bouncy.
-
 .. seealso::
     https://index.ros.org/doc/ros2/Launch-system/
 """
 
 from launch import LaunchDescription
 import launch_ros.actions
+import os
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        launch_ros.actions.Node(
-            package='px4_ros_com', node_executable='sensor_combined_listener', output='screen'),
-    ])
+    if os.environ['ROS_DISTRO'] != "galactic" and os.environ['ROS_DISTRO'] != "rolling":
+        return LaunchDescription([
+            launch_ros.actions.Node(
+                package='px4_ros_com', node_executable='sensor_combined_listener', output='screen'),
+        ])
+    else:
+        return LaunchDescription([
+            launch_ros.actions.Node(
+                package='px4_ros_com', executable='sensor_combined_listener', output='screen'),
+        ])

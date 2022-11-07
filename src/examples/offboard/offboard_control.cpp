9a794657ba79a51e65c1ba9f56eea5b576326e7c
Beniamino Pozzan <beniamino.pozzan@phd.unipd.it>
2022-11-07 15:41:25 -0800

    Revert "Match message definition for TrajectorySetpoint"
    This reverts commit 042d41ab66f25e161cf1cd449ba06727edfdc566.
    to match version v1.13
    
    Signed-off-by: Beniamino Pozzan <beniamino.pozzan@phd.unipd.it>

--- src/examples/offboard/offboard_control.cpp
+++ src/examples/offboard/offboard_control.cpp
@@ -172,7 +172,9 @@ void OffboardControl::publish_offboard_control_mode() const {
 void OffboardControl::publish_trajectory_setpoint() const {
 	TrajectorySetpoint msg{};
 	msg.timestamp = timestamp_.load();
-	msg.position = {0.0, 0.0, -5.0};
+	msg.x = 0.0;
+	msg.y = 0.0;
+	msg.z = -5.0;
 	msg.yaw = -3.14; // [-PI:PI]
 
 	trajectory_setpoint_publisher_->publish(msg);
